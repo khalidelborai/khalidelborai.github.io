b88e44dc1b4b725885968382a6d48a60bf148536
Khalid Elborai <elboraikhalid@gmail.com>
2023-06-04 01:36:56 +0300

    Images ?

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -3,8 +3,14 @@ import Image from "next/image";
 import React from "react";
 import Pre from "./pre";
 
+// ImageLoader for exporting to static HTML
+export function imageLoader({ src, _width, _quality }: any) {
+  return `${src}`;
+}
 function RoundedImage(props: React.ComponentPropsWithoutRef<typeof Image>) {
-  return <Image className="rounded-lg" {...props} alt={props.alt} />;
+  return <Image className="rounded-lg" {...props} alt={props.alt} loader={
+    imageLoader
+  } />;
 }
 
 const components = {
